918bdcab4cc51eef66af42ee9409b879acf18995
YoonCode <yooncode>
2022-10-27 00:29:33 +0900

    add card and card_edit_form

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Editor from '../editor/editor'
 import Footer from '../footer/footer'
@@ -7,8 +7,42 @@ import Preview from '../preview/preview'
 import styles from './maker.module.css'
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Sangjin',
+      company: 'Tooti',
+      theme: 'dark',
+      title: 'Front-end Engineer',
+      email: 'coderyoon@gmail.com',
+      message: 'go for it',
+      fileName: 'sangjin',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Jinsol',
+      company: 'PaPa Mobility',
+      theme: 'light',
+      title: 'Accountant',
+      email: 'jinsol@gmail.com',
+      message: 'go for it',
+      fileName: 'jinsol',
+      fileURL: null,
+    },
+    {
+      id: '3',
+      name: 'Eric',
+      company: 'Winnersmanner',
+      theme: 'colorful',
+      title: 'Product Manager',
+      email: 'eric@gmail.com',
+      message: 'go for it',
+      fileName: 'Eric',
+      fileURL: null,
+    },
+  ])
   const navigate = useNavigate()
-
   const onLogout = () => {
     authService.logout()
   }
@@ -23,8 +57,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
